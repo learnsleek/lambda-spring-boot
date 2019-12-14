ce1cb1c32d3b4614452737fecaa1d746b4fe0001
amit <mailamiton@gmail.com>
2019-12-14 10:58:35 +0530

    Lamda spring boot

---
+++ src/main/java/com/moneyclub/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.moneyclub.controller;
+
+import com.moneyclub.business.model.Color;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class AuthController {
+	
+	@RequestMapping(value="/getcolor2", method = RequestMethod.GET)
+	public List<Color> GetSomething(){
+		return Arrays.asList(
+				new Color("getcolor2"),
+				new Color("Indigo"),
+				new Color("Blue"),
+				new Color("Green"),
+				new Color("Yellow"),
+				new Color("Orange"),
+				new Color("Red")
+				);
+	}
+
+}
